902d164568ff5c10fdc046123910d2701792216d
jb5595 <jacobberman1995@gmail.com>
2018-11-30 11:06:26 -0500

    added form for answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -5,4 +5,16 @@ class AnswersController < ApplicationController
     self.expert
   end
 
+  def create
+    byebug
+    @answer = Answer.create(answer_params)
+    render json: @answer, status: 200
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:content, :question_id, :expert_id)
+  end
+
 end
