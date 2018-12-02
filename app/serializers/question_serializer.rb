af1d2a7bfcb0f8d7f9f315d779bd212ca933d2cc
jb5595 <jacobberman1995@gmail.com>
2018-12-02 14:19:43 -0500

    commit before adding profile images and coverphotos to experts

--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -1,5 +1,5 @@
 class QuestionSerializer < ActiveModel::Serializer
-  attributes :id, :question, :question_details, :upvote_score
+  attributes :id, :question, :question_details, :upvote_score, :created_at
   has_many :answers
   has_many :tags
   belongs_to :user
